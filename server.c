b4e550bca50c403fc51929981f1e6fb5048fd7e9
Egor <e.makrushin15@gmail.com>
2022-09-09 20:06:01 +0300

    added 3rd server message

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #define PROJECT_ID 8841
 
 int main() {
+	char buf[1024];
 	
 	system("touch /tmp/lab5");
 	key_t ipckey = ftok("/tmp/lab5", PROJECT_ID);
@@ -18,6 +19,16 @@ int main() {
 		long type;
 		char text[8192];
 	} message;
+	
+	
+	FILE *hostname = popen("hostname", "r");
+    fread(buf, 1, sizeof(buf), hostname);
+    pclose(hostname);
+    
+	message.type = 3;
+    strcpy(&message.text, buf);
+    msgsnd(mq_id, &message, strlen(message.text), 0);
+	
 	
 	return 0;
 }
