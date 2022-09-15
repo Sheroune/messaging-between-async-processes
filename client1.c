1887ed9c16e048ad45710c880c2f8faf99f1e64b
positronic-flex <e.makrushin15@gmail.com>
2022-09-16 01:44:41 +0300

    fixed tabulation/typos

--- client1.c
+++ client1.c
@@ -4,60 +4,61 @@
 #define PROJECT_ID 8841
 
 int main() {
-	
+
 	key_t ipckey;
 
-    while(1) {
-        ipckey = ftok("/tmp/lab5", PROJECT_ID);
-        
+	while(1) {
+		ipckey = ftok("/tmp/lab5", PROJECT_ID);
+
 		if(ipckey == -1) {
-            printf("No messages\n");
-            sleep(1);
-        }
-		else	break;
-    }
-	
+			printf("No messages\n");
+			sleep(1);
+		}
+		else {
+			break;
+		}
+	}
+
 	//get message query id
 	int mq_id = msgget(ipckey, 0);
-    if (mq_id == -1) {
-        printf("Cannot open message query\n");
-        exit(1);
-    }
-	
-	struct
-    {
+	if(mq_id == -1) {
+		printf("Cannot open message query\n");
+		exit(1);
+   }
+
+	struct {
         long type;
         char text[8192];
     } message;
-	
 
-	int st = msgrcv(mq_id, &message, sizeof(message.text), 1, 0);
-	
+
+	int st = msgrcv(mq_id, &message, sizeof(message.text), 2, 0);
+
 	printf("Message type: %ld\n", message.type);
-	printf("Message content:\n%s", &message.text);
-	
+	printf("Message content: %s", &message.text);
+
 	char *command;
-    int s = asprintf(&command, "ls */ -lR | grep \"^-\" | tr -s \" \" \"\t\" | cut -f 3,9");
-    if (s < 0) {
-        printf("Can't print");
-    }
-	
+	int s = asprintf(&command, "ls */ -lR | grep \"^-\" | tr -s \" \" \"\t\" | cut -f 3,9");
+   if(s < 0) {
+		printf("Can't print");
+	}
+
 	printf("\nOwner:\tFile:\n");
-	
+
 	int code;
-    if (fork() == 0)       // execute action in parallel process
-    {
-        execl("/bin/sh", "sh", "-c", command, (char *) 0);
-    } else {
-        // wait until child process die
-        wait(&code);
-    }
-	
-	
+	if(fork() == 0) {
+   	execl("/bin/sh", "sh", "-c", command, (char *) 0);
+   }
+	else {
+   	// wait until child process die
+		wait(&code);
+   }
+
+
 	// "ls */ -lR | grep "^-" | tr -s " " "\t" | cut -f 3,9"
-	
+
 	// with directories
 	// "ls */ -lR | grep "^-\|:$" | tr -s " " "\t""
-	
+
 	return 0;
 }
--- server.c
+++ server.c
@@ -5,65 +5,58 @@
 
 int main() {
 	char buf[8192];
-	
+
 	system("touch /tmp/lab5");
 	key_t ipckey = ftok("/tmp/lab5", PROJECT_ID);
-	
+
 	int mq_id = msgget(ipckey, IPC_CREAT | 0600);
-    if (mq_id == -1) {
-        printf("Cannot open message query");
-        return -1;
-    }
-	
+	if (mq_id == -1) {
+		printf("Cannot open message query");
+		return -1;
+   }
+
 	struct {
 		long type;
 		char text[8192];
 	} message;
-	
-	
+
+
 	// 1st message
-	FILE *dirs = popen("tree -ad | head --lines=-2 | sed '1d'", "r");
-    fread(buf, 1, sizeof(buf), dirs);
-    pclose(dirs);
-	
-	// printf("%s\n", &buf);
-	
-    message.type = 1;
-    strcpy(&message.text, buf);
-    msgsnd(mq_id, &message, strlen(message.text), 0);
-    
+	FILE *dirs = popen("tree -d | head --lines=-2 | sed '1d'", "r");
+   fread(buf, 1, sizeof(buf), dirs);
+   pclose(dirs);
+
+	message.type = 1;
+   strcpy(&message.text, buf);
+   msgsnd(mq_id, &message, strlen(message.text), 0);
+
 	memset(buf, 0, 8192);
-	
-	
+
+
 	// 2nd message
-	FILE *files = popen("expr $(ls -laR | grep \"^-\" | wc -l) - $(ls -la | grep \"^-\" | wc -l)", "r");
-    fread(buf, 1, sizeof(buf), files);
-    pclose(files);
-	
-	// printf("%s\n", &buf);
-	
-    message.type = 2;
-    strcpy(&message.text, buf);
-    msgsnd(mq_id, &message, strlen(message.text), 0);
-    
+	FILE *files = popen("expr $(ls -lR | grep \"^-\" | wc -l) - $(ls -l | grep \"^-\" | wc -l)", "r");
+   fread(buf, 1, sizeof(buf), files);
+   pclose(files);
+
+   message.type = 2;
+   strcpy(&message.text, buf);
+   msgsnd(mq_id, &message, strlen(message.text), 0);
+
 	memset(buf, 0, 8192);
-	
-	
+
+
 	// 3rd message
 	FILE *hostname = popen("hostname", "r");
-    fread(buf, 1, sizeof(buf), hostname);
-    pclose(hostname);
-	
-	// printf("%s\n", &buf);
-    
+   fread(buf, 1, sizeof(buf), hostname);
+   pclose(hostname);
+
 	message.type = 3;
-    strcpy(&message.text, buf);
-    msgsnd(mq_id, &message, strlen(message.text), 0);
-	
+   strcpy(&message.text, buf);
+   msgsnd(mq_id, &message, strlen(message.text), 0);
+
 	// ???
 	//printf("Deleting messages query\n");
-    //msgctl(mq_id, IPC_RMID, NULL);
-	
+   //msgctl(mq_id, IPC_RMID, NULL);
+
 	return 0;
 }
-
