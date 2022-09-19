ee9c1429f904a8b570b780b6cfdd13a81fcca259
positronic-flex <e.makrushin15@gmail.com>
2022-09-20 02:52:11 +0300

    server receive messages from the 2nd client

--- server.c
+++ server.c
@@ -9,9 +9,10 @@ int main() {
 	system("touch /tmp/lab5");
 	key_t ipckey = ftok("/tmp/lab5", PROJECT_ID);
 
-	int mq_id = msgget(ipckey, IPC_CREAT | 0600);
+	printf("[INFO] Creating message query\n");
+	int mq_id = msgget(ipckey, IPC_CREAT | 0666);
 	if (mq_id == -1) {
-		printf("Cannot open message query");
+		printf("Error: cannot open message query");
 		return -1;
    }
 
@@ -22,6 +23,7 @@ int main() {
 
 
 	// 1st message
+	printf("[INFO] Sending 1st message\n");
 	FILE *dirs = popen("tree -d | head --lines=-2 | sed '1d'", "r");
    fread(buf, 1, sizeof(buf), dirs);
    pclose(dirs);
@@ -34,6 +36,7 @@ int main() {
 
 
 	// 2nd message
+	printf("[INFO] Sending 2nd message\n");
 	FILE *files = popen("expr $(ls -lR | grep \"^-\" | wc -l) - $(ls -l | grep \"^-\" | wc -l)", "r");
    fread(buf, 1, sizeof(buf), files);
    pclose(files);
@@ -46,6 +49,7 @@ int main() {
 
 
 	// 3rd message
+	printf("[INFO] Sending 3rd message\n");
 	FILE *hostname = popen("hostname", "r");
    fread(buf, 1, sizeof(buf), hostname);
    pclose(hostname);
@@ -54,9 +58,18 @@ int main() {
    strcpy(&message.text, buf);
    msgsnd(mq_id, &message, strlen(message.text), 0);
 
+	memset(&message.text, 0, 8192);
+
+	printf("[INFO] Waiting for messages from the clients\n");
+	int st = msgrcv(mq_id, &message, sizeof(message.text), 4, 0);
+	printf("[INFO] Message received\n\n");
+
+	printf("Message type: %ld\n", message.type);
+	printf("Message content:\n%s\n", &message.text);
+
 	// ???
-	//printf("Deleting messages query\n");
-   //msgctl(mq_id, IPC_RMID, NULL);
+	printf("[INFO] Deleting message query\n");
+   msgctl(mq_id, IPC_RMID, NULL);
 
 	return 0;
 }
